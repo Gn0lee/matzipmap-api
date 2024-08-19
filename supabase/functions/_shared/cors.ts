f99d1c4857df0638ffcaa10c8d2b9056f2cd5057
Gn0lee <jinho605@gmail.com>
2024-08-19 23:20:48 +0900

    fix: 로컬에서 oauth 설정

--- supabase/functions/_shared/cors.ts
+++ supabase/functions/_shared/cors.ts
@@ -1,6 +1,6 @@
 export const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Headers":
-    "authorization, x-client-info, apikey, content-type, uid",
+    "authorization, x-client-info, apikey, content-type, access-token, refresh-token",
   "Access-Control-Allow-Methods": "*",
 };
