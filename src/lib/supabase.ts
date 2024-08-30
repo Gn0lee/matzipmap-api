fb513503813eb4290ccbb4eb439606eff6c3a92f
Gn0lee <jinho605@gmail.com>
2024-08-31 00:43:48 +0900

    test: 쿠키 설정 테스트

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,17 +1,24 @@
-import {createServerClient, parseCookieHeader, serializeCookieHeader} from '@supabase/ssr';
-import {  Request, Response } from 'express';
+import { createServerClient, parseCookieHeader, serializeCookieHeader } from '@supabase/ssr';
+import { Request, Response } from 'express';
 
-const createClient = ({req, res}: {res: Response; req: Request}) => createServerClient(process.env.SUPABASE_URL ?? '', process.env.SUPABASE_ANON_KEY ?? '', {
-	cookies: {
-		getAll ()  {
-			return parseCookieHeader(req.headers.cookie ?? '');
+const createClient = ({ req, res }: { res: Response; req: Request }) =>
+	createServerClient(process.env.SUPABASE_URL ?? '', process.env.SUPABASE_ANON_KEY ?? '', {
+		cookies: {
+			getAll() {
+				console.log('getAll');
+				console.log(req.headers.cookie ?? '');
+				return parseCookieHeader(req.headers.cookie ?? '');
+			},
+			setAll(cookiesToSet) {
+				cookiesToSet.forEach(({ name, value, options }) => {
+					console.log('cookiesToSet');
+
+					console.log(name, value, options);
+
+					res.appendHeader('Set-Cookie', serializeCookieHeader(name, value, options));
+				});
+			},
 		},
-		setAll(cookiesToSet) {
-			cookiesToSet.forEach(({ name, value, options }) =>
-				res.appendHeader('Set-Cookie', serializeCookieHeader(name, value, options))
-			)
-		},
-	}
-});
+	});
 
 export default createClient;
