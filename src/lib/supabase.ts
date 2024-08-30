ea1fa22e2f9c17af08a96450176129212d1ae9cc
Gn0lee <jinho605@gmail.com>
2024-08-31 03:08:27 +0900

    fix: 패키지 업데이트

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -5,16 +5,10 @@ const createClient = ({ req, res }: { res: Response; req: Request }) =>
 	createServerClient(process.env.SUPABASE_URL ?? '', process.env.SUPABASE_ANON_KEY ?? '', {
 		cookies: {
 			getAll() {
-				console.log('getAll');
-				console.log(req.headers.cookie ?? '');
 				return parseCookieHeader(req.headers.cookie ?? '');
 			},
 			setAll(cookiesToSet) {
 				cookiesToSet.forEach(({ name, value, options }) => {
-					console.log('cookiesToSet');
-
-					console.log(name, value, options);
-
 					res.appendHeader('Set-Cookie', serializeCookieHeader(name, value, options));
 				});
 			},
